d510f8559bdfb8b8f88d98e557707d549bdb0760
unknown <adamzfriday@gmail.com>
2025-04-24 13:59:15 +0100

    More changes made to dogage.js code

--- JS/dogage.js
+++ JS/dogage.js
@@ -1,3 +1,15 @@
-let age = prompt("Enter your age:");
+// let age = prompt("Dog age checker. Enter your human age to find out:");
+// age = Number(age);
+// let dogyrs = alert("You are "+ age*7 + " in dog years" );
 
+let age = prompt("Dog age checker. Enter your human age to find out:");
 age = Number(age);
+let dogyrs = alert("You are " + age*7 + " in dog years");
+
+if (age <= 0) {
+    alert("You have to be one year old or above");
+}else if (!age) {
+    alert ("Please enter your age in numbers");
+}
+
+
